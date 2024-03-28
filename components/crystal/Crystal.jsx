15a552771a0cc120e55513e2ad86fb048603a889
Michel <soinovmich@yandex.ru>
2024-03-28 11:48:30 +0300

    comm28.03.2

--- components/crystal/Crystal.jsx
+++ components/crystal/Crystal.jsx
@@ -7,7 +7,7 @@ const Crystal = () => {
         <div>
             <div className={styles.textmain}>
                 <div className={styles.cont1}>
-                    <Image src="/home5-s-2-1.png" width={466} height={486} alt="img_home"/>
+                    <Image src="/home5-s-2-1.png" width={466} height={486} alt="img home"/>
                 </div>
                 <div className={styles.cont2}>
                     <div className={styles.cont3}>
@@ -15,7 +15,7 @@ const Crystal = () => {
                             Create, Sell well & Collect Your Best<br/> Very Fast NFTs.
                         </div>
                         <div className={styles.text2}>
-                            Create, Sell well & Collect Your Best Very Fast NFTs.The purpose of lorem ipsum is to create a<br/> natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the<br/> layout. A practice not without controversy, laying out pages with meaningless
+                            Create, Sell well & Collect Your Best Very Fast NFTs.The purpose of lorem ipsum is to create a<br/> natural looking block of text sentence, paragraph, page, etc. that doesnt distract from the<br/> layout. A practice not without controversy, laying out pages with meaningless
                         </div>
                     </div>
                     <div className={styles.text3}>
